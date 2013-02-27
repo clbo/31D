8f72b3a5cc67a58f59732b0bdb0592a83eb870a3
Claus <clb@rts.dk>
2013-02-27 12:13:57 +0100

    CRUD

---
+++ CRUD_Code_Behind/Delete.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+public partial class Delete : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // opret forbindelsen til database
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ToString());
+        // opret et SqlCommand object
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        //Sql sætningen
+        cmd.CommandText = "DELETE FROM news WHERE n_id = @id";
+        // Parameteret @id i sql sætningen ovenover tilføjes
+        cmd.Parameters.Add("@id", SqlDbType.Int).Value = Request.QueryString["newsid"];
+        // åben forbindelsen til databasen
+        conn.Open();
+        // Udfør sql komandoen
+        cmd.ExecuteNonQuery();
+        // luk forbindelsen til databasen
+        conn.Close();
+
+    }
+}
---
+++ CRUD_Code_Behind/Update.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+public partial class Update : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // SELECT fra Databasen og fyld i TextBoxe
+    }
+
+    protected void Button_update_Click(object sender, EventArgs e)
+    {
+        // opret forbindelsen til databasen
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ToString());
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        //Sql sætningen
+        cmd.CommandText = "UPDATE news SET n_title = @title, n_tekst = @tekst WHERE n_id = @id";
+        // Parametrene @id, @title og @tekst i sql sætningen ovenover tilføjes
+        cmd.Parameters.Add("@id", SqlDbType.Int).Value = Request.QueryString["newsid"];
+        cmd.Parameters.Add("@title", SqlDbType.NVarChar).Value = TextBox_titel.Text;
+        cmd.Parameters.Add("@tekst", SqlDbType.NVarChar).Value = TextBox_tekst.Text;
+        // åben forbindelsen til databasen
+        conn.Open();
+        // Udfør sql komandoen
+        cmd.ExecuteNonQuery();
+        // luk forbindelsen til databasen
+        conn.Close();
+
+    }
+}
