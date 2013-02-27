2ca806b8c4608c7b84f6846b45443c2f1c04bd2e
Claus <clb@rts.dk>
2013-02-27 16:01:05 +0100

    Upload Resize

---
+++ Upload_Resize/Resize.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+public partial class Resize : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Opret et Thumb, baseret på en fil i en mappe
+    /// </summary>
+    /// <param name="Filename">Hvad hedder filen</param>
+    /// <param name="UploadFolder">Hvor er originalen uploadet til</param>
+    /// <param name="bredde">Hvad er bredden på den thumbnail du vil lave</param>
+    /// <param name="thumbuploadfolder">Hvilken mappe skal thumbnailen uploads til</param>
+    private void MakeThumb(string Filename, string UploadFolder, int bredde, string thumbuploadfolder)
+    {
+        // find det uploadede image
+        System.Drawing.Image OriginalImg = System.Drawing.Image.FromFile(Server.MapPath("~/") + UploadFolder + Filename);
+        // "UploadFolder" og "Filename" er parametre ... se mellem paranteserne () efter metodenavnet "MakeThumb" ovenover
+
+        // find højde og bredde på image
+        int originalWidth = OriginalImg.Width;
+        int originalHeight = OriginalImg.Height;
+
+        // bestem den nye bredde på det thumbnail som skal laves
+        int newWidth = bredde; // "bredde" er et parameter .. se mellem paranteserne () efter metodenavnet MakeThumb ovenover
+
+        // beregn den nye højde på thumbnailbilledet
+        double ratio = newWidth / (double)originalWidth;
+        int newHeight = Convert.ToInt32(ratio * originalHeight);
+
+        Bitmap Thumb = new Bitmap(newWidth, newHeight, PixelFormat.Format24bppRgb);
+        Thumb.SetResolution(OriginalImg.HorizontalResolution, OriginalImg.VerticalResolution);
+
+        // Hvis billedet indeholder nogen form for transperans 
+        //(mere eller mindre gennemsigtig, eller en gennemsigtig baggrund) bliver det gjort her
+        Thumb.MakeTransparent();
+
+        // opret det nye billede
+        Graphics ThumbMaker = Graphics.FromImage(Thumb);
+        ThumbMaker.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+        ThumbMaker.DrawImage(OriginalImg,
+            new Rectangle(0, 0, newWidth, newHeight),
+            new Rectangle(0, 0, originalWidth, originalHeight),
+            GraphicsUnit.Pixel);
+
+        // encoding
+        ImageCodecInfo encoder;
+        string fileExt = System.IO.Path.GetExtension(Filename);
+        switch (fileExt)
+        {
+            case ".png":
+                encoder = GetEncoderInfo("image/png");
+                break;
+
+            case ".gif":
+                encoder = GetEncoderInfo("image/gif");
+                break;
+
+            default:
+                // default til JPG 
+                encoder = GetEncoderInfo("image/jpeg");
+                break;
+        }
+
+        EncoderParameters encoderParameters = new EncoderParameters(1);
+        encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, 100L);
+
+        // gem thumbnail i mappen /Images/Uploads/Thumbs/
+        Thumb.Save(Server.MapPath("~") + thumbuploadfolder + Filename, encoder, encoderParameters);
+        // "thumbuploadfolder" og "Filename" er parametre ... se mellem paranteserne () efter metodenavnet "MakeThumb" ovenover
+
+        // Fjern originalbilledet, thumbnail mm, fra computerhukommelsen
+        OriginalImg.Dispose();
+        ThumbMaker.Dispose();
+        Thumb.Dispose();
+
+    }
+
+    private static ImageCodecInfo GetEncoderInfo(String mimeType)
+    {
+        ImageCodecInfo[] encoders = ImageCodecInfo.GetImageEncoders();
+        for (int i = 0; i < encoders.Length; i++)
+        {
+            if (encoders[i].MimeType == mimeType)
+            {
+                return encoders[i];
+            }
+        }
+        return null;
+    }
+
+}
