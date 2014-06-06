700bc87f30d4579e9d051cab3b6dc4e532697482
clbo <clbo@users.noreply.github.com>
2014-06-06 23:06:08 +0200

    Comment in page_Loade

--- CRUD_Code_Behind/Insert.aspx.cs
+++ CRUD_Code_Behind/Insert.aspx.cs
@@ -12,7 +12,7 @@ public partial class Insert : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // comment
     }
     protected void Button_insert_Click(object sender, EventArgs e)
     {
